91ab308b4ae84cc1363a9f0ec1876c593cde7b35
Michael <Michael@10.58.132.161>
2016-11-30 16:51:45 +0100

    SQL script lagt ind i Docs/*

---
+++ CakeWebShop/Docs/Database SQL.sql
@@ -0,0 +1,52 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+ * Author:  Michael
+ * Created: 30-11-2016
+ */
+
+drop database if exists cakeWebShop;
+create database cakeWebShop;
+use cakeWebShop;
+
+create table users
+(
+	userid int(5) primary key auto_increment,
+    firstname varchar(30) not null,
+    lastname varchar(30) not null,
+    email  varchar(50) not null,
+    phone varchar(8) not null,					# tlf.nr. i dk er 8 (kan udvides)
+    address varchar(50) not null,
+    zip varchar(4) not null,					# 4 fordi i dk, grønland og færøerne bruges kun max 4 (kan udvides)
+    password varchar(40) not null				# 40 fordi SHA1 er 40 char lang 
+);
+
+create table shopItems
+(
+	itemid int(5) primary key auto_increment,
+    itemName varchar(150) not null,
+    itemPrice double(3,2) not null
+);
+
+create table orders
+(
+	orderid int(5) primary key auto_increment,
+    userid int(5),								# referere til users(userid)
+    FOREIGN KEY (userid) REFERENCES users(userid),
+    orderStatus int(1),							# 0=ikke behandlet 1=sendt
+    orderDate datetime,
+    orderDeliveryDate datetime
+);
+
+create table orderLines
+(
+	orderLineid int(10) primary key auto_increment,
+    orderid int(5),
+    FOREIGN KEY (orderid) REFERENCES orders(orderid),
+    shopItem int(5),
+    FOREIGN KEY (shopItem) REFERENCES shopItems(itemid),
+    itemPrice double(3,2) not null				# ikke foreign key af historiske orsager.
+);
--- CakeWebShop/src/java/com/cakewebsop/db/DB.java
+++ CakeWebShop/src/java/com/cakewebsop/db/DB.java
@@ -1,14 +1,43 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package com.cakewebsop.db;
+package dataaccess;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 /**
  *
- * @author Michael
+ * @author Thomas Hartmann - tha@cphbusiness.dk created on Nov 16, 2016 
  */
 public class DB {
+    private static final String DRIVER = "com.mysql.jdbc.Driver";
+    private static final String URL = "jdbc:mysql://localhost:3306/cakeWebShop";
+    private static final String USER = "root";
+    private static final String PASSWORD = "root";
+    private static Connection conn = null;
+    
+    public static Connection getConnection(){
+        try {
+            Class.forName(DRIVER);
+            conn = DriverManager.getConnection(URL, USER, PASSWORD);
+        } catch (ClassNotFoundException ex) {
+            ex.printStackTrace();
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        }
+        return conn;
+    }
+    
+//    public static void main(String[] args) throws SQLException {
+//        Statement stmt = getConnection().createStatement();
+//        ResultSet rs = stmt.executeQuery("SELECT * FROM usertable");
+//        while(rs.next()){
+//            int id = rs.getInt("id");
+//            String username = rs.getString("username");
+//            String password = rs.getString("password");
+//            System.out.println(id+":"+username+":"+password);
+//        }
+//    }
     
 }

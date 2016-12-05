6a94b95a4092100fc3e719475873b55d1cee61aa
Frey Clante <fclante@gmail.com>
2016-12-05 16:15:49 +0100

    Added UserMapper.java and User.Java - No implementation done yet.

--- CakeWebShop/src/java/models/User.java
+++ CakeWebShop/src/java/models/User.java
@@ -3,12 +3,12 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package controller;
+package models;
 
 /**
  *
  * @author Michael
  */
-public class Login {
+public class User {
     
 }

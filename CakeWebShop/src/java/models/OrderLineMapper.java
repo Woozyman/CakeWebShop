45c02721c49e72f8ec5d1817ba44fe2630b71823
Frey Clante <fclante@gmail.com>
2016-12-06 19:43:20 +0100

    Made getOrderLines() in OrderLineMapper.java

--- CakeWebShop/src/java/models/OrderLineMapper.java
+++ CakeWebShop/src/java/models/OrderLineMapper.java
@@ -6,16 +6,50 @@
 package models;
 
 import dataaccess.DB;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  *
  * @author freyb
  */
 public class OrderLineMapper {
+
     private DB db;
-    
-    public OrderLineMapper(){
+
+    public OrderLineMapper() {
         this.db = new DB();
     }
-    
+
+    public List<OrderLine> getOrderLines(int orderId) {
+        List<OrderLine> orderLines = new ArrayList();
+
+        try {
+            String query = "SELECT orderid, shopItemid, itemPrice FROM orderlines"
+                    + "WHERE orderid = ? ";
+            PreparedStatement ps = db.getConnection().prepareStatement(query);
+            ps.setInt(1, orderId);
+
+            ResultSet rs = ps.executeQuery();
+            db.closeConnection();
+
+            while (rs.next()) {
+                int orderIdNum = rs.getInt("orderid");
+                int shopItemId = rs.getInt("shopItemid");
+                double itemPrice = rs.getDouble("itemPrice");
+
+                OrderLine orderLine = new OrderLine(orderIdNum, shopItemId, itemPrice);
+                orderLines.add(orderLine);
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return orderLines;
+    }
+
 }

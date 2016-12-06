18e2084c70cadd05d3d46b637cb7c53a9e579274
Frey Clante <fclante@gmail.com>
2016-12-06 16:45:47 +0100

    Worked on ShopItemMapper.java Just implemented various methods.

--- CakeWebShop/src/java/models/ShopItem.java
+++ CakeWebShop/src/java/models/ShopItem.java
@@ -5,29 +5,40 @@
  */
 package models;
 
+import java.sql.Date;
+
 /**
  *
  * @author freyb
  */
 public class ShopItem {
-    
+
     private int itemId;
     private String itemName;
     private String itemPicture;
     private double itemPrice;
-    
+    private Date discontinuedDate;
+
     public ShopItem(){}
-    
-    public ShopItem(String name, String picture, double price){
+
+    public ShopItem(String name, String picture, double price) {
         this.itemName = name;
         this.itemPicture = picture;
         this.itemPrice = price;
     }
 
+    public Date getDiscontinuedDate() {
+        return discontinuedDate;
+    }
+
+    public void setDiscontinuedDate(Date discontinuedDate) {
+        this.discontinuedDate = discontinuedDate;
+    }
+
     public int getItemId() {
         return itemId;
     }
-   
+
     public String getItemName() {
         return itemName;
     }
@@ -51,7 +62,5 @@ public class ShopItem {
     public void setItemPrice(double itemPrice) {
         this.itemPrice = itemPrice;
     }
-    
-    
-    
+
 }
--- CakeWebShop/src/java/models/ShopItemMapper.java
+++ CakeWebShop/src/java/models/ShopItemMapper.java
@@ -6,8 +6,13 @@
 package models;
 
 import dataaccess.DB;
+import java.sql.Date;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -16,54 +21,125 @@ import java.util.List;
  * @author freyb
  */
 public class ShopItemMapper {
+
     private DB db;
-    
-    public ShopItemMapper(){
+
+    public ShopItemMapper() {
         this.db = new DB();
     }
-    
-    public void addItem(ShopItem item){
-        
-    }
-    
-    public boolean removeItem(int id){
-        return false;
-    }
-    
-    //TODO Implement the rest
-    public ShopItem getItem(int id){
-        ShopItem item = new ShopItem();
-        
-        String query = "";      
-        
+
+    public void addItem(ShopItem item) {
         try {
-             PreparedStatement ps = db.getConnection().prepareStatement(query);
-             
-            boolean result = ps.execute();
+            String name = item.getItemName();
+            String picture = item.getItemPicture();
+            double price = item.getItemPrice();
+
+            String query = "INSERT INTO shopitems(name, picture, price)"
+                    + "VALUES(?,?,?)";
+
+            PreparedStatement ps = db.getConnection().prepareStatement(query);
+            ps.setString(1, name);
+            ps.setString(2, picture);
+            ps.setDouble(3, price);
+
+            ps.execute();
+            db.closeConnection();
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void setDiscontinuedDate(ShopItem item, String date) {
+        DateFormat formatter = new SimpleDateFormat("dd-MM-yyyy");
+        try {
+            Date discontinuedDate = new Date(formatter.parse(date).getTime());
+            item.setDiscontinuedDate(discontinuedDate);
+
+            try {
+                String query = "SET discontinuedate=? WHERE itemid = ? ";
+
+                PreparedStatement ps = db.getConnection().prepareStatement(query);
+                ps.setDate(1, discontinuedDate);
+                ps.setInt(2, item.getItemId());
+
+                int result = ps.executeUpdate();
+                db.closeConnection();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void setDiscontinuedDateNull(ShopItem item) {
+        try {
+            String query = "SET discontinuedate=null WHERE itemid = ? ";
+
+            PreparedStatement ps = db.getConnection().prepareStatement(query);
+            ps.setInt(1, item.getItemId());
+
+            int result = ps.executeUpdate();
             db.closeConnection();
         } catch (SQLException e) {
-            
+            e.printStackTrace();
         }
-        
-        return item;
     }
-    
-    //TODO Implement this
-    public List<ShopItem> getAllItems(){
-        List<ShopItem> items = new ArrayList();
-        
-        String query = "";
-        
+
+    //TODO Implement the rest
+    public ShopItem getItem(int id) {
+        ShopItem item = null;
+
+        String query = "SELECT itemName, itemPicture, itemPrice FROM shopitems "
+                + "WHERE itemid = ? ";
+
         try {
             PreparedStatement ps = db.getConnection().prepareStatement(query);
-            
-            ps.executeQuery();
+            ps.setInt(1, id);
+
+            boolean result = ps.execute(); //Returns true if result is a ResultSet and false if no result
             db.closeConnection();
-                    
+            
+            if (result) {
+                ResultSet rs = ps.getResultSet();
+                item = new ShopItem(rs.getString("itemName"), rs.getString("itemPicture"), rs.getDouble("itemPrice"));
+            }else{
+                return null;
+            }
+
+            
         } catch (SQLException e) {
+            e.printStackTrace();
         }
-        
-        return items;
+        return item;
     }
-    
+
+    //TODO Implement this
+    public List<ShopItem> getAllItems() {
+        List<ShopItem> shopItems = new ArrayList();
+
+        try {
+            String query = "SELECT itemName, itemPicture, itemPrice FROM shopitems";
+            PreparedStatement ps = db.getConnection().prepareStatement(query);
+
+            ResultSet rs = ps.executeQuery();
+            db.closeConnection();
+
+            while (rs.next()) {
+                String name = rs.getString("itemName");
+                String picture = rs.getString("itemPicture");
+                double price = rs.getDouble("itemPrice");
+
+                ShopItem item = new ShopItem(name, picture, price);
+                shopItems.add(item);
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return shopItems;
+    }
+
 }

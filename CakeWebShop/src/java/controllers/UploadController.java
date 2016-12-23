3c2ca214b6f5951d354756fbb25bd69a557ee513
Frey Clante, Jens Kolby, Michael Hansen <fclante@gmail.com>
2016-12-23 17:39:34 +0100

    Final Commit!. DO NOT ALTER!!!

--- CakeWebShop/src/java/controllers/UploadController.java
+++ CakeWebShop/src/java/controllers/UploadController.java
@@ -98,7 +98,7 @@ public class UploadController extends HttpServlet {
 
             // Upload path // Change to local path of WebApp
             //String Dir = "/Users/Michael/Desktop/CPHBusiness/05 Webprogrammering ServerSide/NetBeans/CakeWebShop/CakeWebShop/web/images";
-            String Dir = "E:\\Development\\School\\WebServer\\EksamensProjekt\\CakeWebShop\\CakeWebShop\\web\\images";
+            String Dir = "local PATH TO /images folder in Project";
             boolean saved = false;
 
             for (FileItem item : files) {
